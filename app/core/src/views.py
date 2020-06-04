ad87fc600ee7f4655a047126900d5ba0af5e18e9
Stefania <stefania1219@hotmail.com>
2020-06-04 00:56:55 -0500

    Edicion de estilos

--- app/core/src/views.py
+++ app/core/src/views.py
@@ -13,12 +13,6 @@ class HomePageView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'tituloIni':'Los saluda Stefania Giraldo.', 'titulo2':'Clases de python.'})
 
-class NosotrosPageView(TemplateView):
-
-    template_name = "nosotros.html"
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'titulous':'Acerca de Nosotras','subti':'ADSI','nomEstu1':'Stefanía Giraldo Monsalve','nomEstu2':'Daniela Zapata Gutierrez','nomEstu3':'Karen Vásquez Hernández','parr1':'Se encarga de desarrollar el sistema operativo de coinsa y de la realización de pruebas de calidad al codigo varias veces para garantizar su eficiencia.','parr2':'Se encarga de la administración de la base de datos y que todo fluya correctamente.','parr3':'Se encarga de diseñar la arquitectura del proyecto, se asegura que todos los requisitos propuestos sean cumplidos en el equipo de trabajo.'})
 
 # Vista basada en función:
 def contacto(request):
