f739452700ff8d88a33e85df844c3abf094810b3
Stefania <stefania1219@hotmail.com>
2020-06-17 18:23:26 -0500

    Actualización

--- app/core/userdata/views.py
+++ app/core/userdata/views.py
@@ -10,3 +10,4 @@ class NosotrosPageView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'titulous':'Acerca de Nosotras','subti':'ADSI','nomEstu1':'Stefanía Giraldo Monsalve','nomEstu2':'Daniela Zapata Gutierrez','nomEstu3':'Karen Vásquez Hernández','parr1':'Se encarga de desarrollar el sistema operativo de coinsa y de la realización de pruebas de calidad al codigo varias veces para garantizar su eficiencia.','parr2':'Se encarga de la administración de la base de datos y que todo fluya correctamente.','parr3':'Se encarga de diseñar la arquitectura del proyecto, se asegura que todos los requisitos propuestos sean cumplidos en el equipo de trabajo.'})
 
+        
